15eac9cb20da80ab15d3428b218fab48184dbfee
ColinChen25 <47988458+ColinChen25@users.noreply.github.com>
2019-04-12 16:13:12 -0500

    Add files via upload

--- MyFirstJavaFX/application/Main.java
+++ MyFirstJavaFX/application/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -11,13 +12,18 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 
-
+/**
+ * Main class for creating a test JavaFX program
+ * @author Colin Mercer
+ *
+ */
 public class Main extends Application {
   @Override
   public void start(Stage primaryStage) {
     try {
       Label greetingLabel = new Label("Hello World!");
       Label label1 = new Label("CS400 MyFirstJavaFX");
+      Button button1 = new Button("Done");
       ImageView myFace = new ImageView(new Image("98.12.15.jpg"));
       myFace.setFitWidth(500);
       myFace.setPreserveRatio(true);
@@ -28,6 +34,7 @@ public class Main extends Application {
           new ComboBox(FXCollections.observableArrayList("Option 1", "Option 2", "Option 3")));
       root.setCenter(myFace);
       root.setRight(new TextField());
+      root.setBottom(button1);
       scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
       primaryStage.setScene(scene);
       primaryStage.setTitle("Colin's First JavaFX program");
@@ -37,6 +44,10 @@ public class Main extends Application {
     }
   }
 
+  /**
+   * Main method for starting up JavaFX
+   * @param args
+   */
   public static void main(String[] args) {
     launch(args);
   }
